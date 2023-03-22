16fab9ecb87bb3cca06c6a448ecda1dbfc2b5afc
Madusha <madusha.20200573@iit.ac.lk>
2023-03-22 17:53:24 +0530

    "second commit"

--- server/index.js
+++ server/index.js
@@ -14,6 +14,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 // Default
 app.get("/", (req, res) => {
   res.json("Hello to my app");
